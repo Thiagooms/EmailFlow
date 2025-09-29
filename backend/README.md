4ca15ab5eace66c7848ca47b3381b04a94f6d0a4
Thiago <thiagopaulista50@gmail.com>
2025-09-29 03:18:22 -0300

    feat: create Flask api and logical separation for service layer

---
+++ backend/README.md
@@ -0,0 +1,11 @@
+<div align="center">
+
+EmailFlow: Analisador de Emails com IA
+---
+<p>Uma aplicação web que utiliza a API do Google Gemini para classificar o conteúdo de emails e sugerir respostas, construída com React, Flask e implantada na AWS.</p>
+
+[![My Skills](https://skillicons.dev/icons?i=python,react,aws,flask)](https://skillicons.dev)
+
+![alt text](image.png)
+
+</div>
--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
+from .services.service import sort_email
 
 app = Flask(__name__)
 CORS(app)
@@ -7,20 +8,15 @@ CORS(app)
 @app.route("/api/analyze", methods=['POST'])
 def analyze_email():
 
-    email_text = request.json.get('text')
+    data = request.get_json()
+    email_text = data.get('text')
 
     if not email_text:
         return jsonify({"error": "Nenhum texto fornecido"}), 400
     
-    print(f"Email recebido para análise: {email_text[:50]}...")
+    result = sort_email(email_text)
 
-    response_data = {
-        "categoria": "Produtivo (Teste)",
-        "resposta_sugerida": "Esta é uma resposta de teste gerada pela API Flask."
-    }
+    if "error" in result:
+        return jsonify(result), 500
 
-    return jsonify(response_data)
-
-@app.route("/")
-def hello():
-    return "<h1>Deploy feito a partir do GitHub!</h1>"
+    return jsonify(result)
